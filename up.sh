ab215a7b73c9495cc29f8b9b903b22792ebb7568
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2024-07-09 09:49:27 +0200

    test

---
+++ up.sh
@@ -0,0 +1,28 @@
+git pull
+git add .
+git commit -m "$*"
+git push
+
+# if permission is denied on Macbook, do that:
+
+
+# You can open the terminal (press Ctrl + Alt + T) and cd to the target directory:
+
+# cd /path/to/target
+# To give the file "the_file_name" execute permission (if the file-system allows you with the RW rights):
+
+# chmod +x the_file_name
+
+
+
+
+# Opitonal informations
+# Github ssh key cat ~/.ssh/id_rsa.pub
+
+# Richtige Ausfuehrung von git zu git hub
+# Git init
+# Git add .
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/Kakowa18/joinTest.git
+# git push -u origin main
